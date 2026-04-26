80b5ac88a1bb0ba84c6201f335aaff0d841884ec
Vahagn Vardanyan <vahagn.vardanyan2@picsart.com>
2026-04-27 02:37:23 +0400

    first commit

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,12 +1,9 @@
-import type { Metadata } from 'next';
-
 import { Inter } from 'next/font/google';
 import { notFound } from 'next/navigation';
 import { NextIntlClientProvider } from 'next-intl';
-import { getMessages, getTranslations } from 'next-intl/server';
+import { getMessages } from 'next-intl/server';
 
 import { routing } from '@/i18n/routing';
-import { BASE_URL } from '@/constants/configs';
 import { QueryProvider } from '@/providers/QueryProvider';
 import '../globals.css';
 
@@ -16,33 +13,6 @@ const inter = Inter({
   display: 'swap',
 });
 
-export const generateMetadata = async ({
-  params,
-}: {
-  params: Promise<{ locale: string }>;
-}): Promise<Metadata> => {
-  const { locale } = await params;
-  const translate = await getTranslations({ locale });
-
-  return {
-    title: {
-      default: translate('seoTitle'),
-      template: `%s | ${translate('appTitle')}`,
-    },
-    description: translate('seoDescription'),
-    keywords: translate('seoKeywords'),
-    alternates: {
-      canonical: `${BASE_URL}/${locale}`,
-    },
-    openGraph: {
-      title: translate('seoTitle'),
-      description: translate('seoDescription'),
-      locale,
-      type: 'website',
-      url: `${BASE_URL}/${locale}`,
-    },
-  };
-};
 
 const LocaleLayout = async ({
   children,
